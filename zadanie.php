d9b86a8942d02a797fb0751772a653d866424005
cgt19kaprzykp <piotrekkaprzyk62@gmail.com>
2022-09-20 10:24:30 +0200

    zadanie

---
+++ zadanie.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$Integer1= 21
+?>
+<?php
+$Integer2=37
+?>
+<?php
+$Float1=4.2
+?>
+<?php
+$Float2=3.3
+?>
+<?php
+echo($Integer1).var_dump(is_int($Integer1))
+?>
+<?php
+echo($Integer2).var_dump(is_int($Integer2))
+?>
+<?php
+echo($Float1).var_dump(is_int($Float1))
+?>
+<?php
+echo($Float2).var_dump(is_int($Float2))
+?>
+
+</body>
+</html>
